d0b42a6cf022b3b2df031d690c863c770372d160
Nirav Domah <nir.domah@gmail.com>
2024-09-23 16:05:01 +0400

    Add logging results to file

--- notebook2.py
+++ notebook2.py
@@ -5,9 +5,9 @@ from modules.tsp_file_parser.tsp_file_parser import TSP
 
 # problemName = "ulysses16.tsp"
 # problemName = "a280.tsp"
-problemName = "berlin52.tsp"
-currentFolderPath = os.path.abspath("")
-tspFilePath = os.path.join(currentFolderPath, f"data\\{problemName}")
+problem_name = "berlin52.tsp"
+currentFolderPath: str = os.path.abspath("")
+tspFilePath = os.path.join(currentFolderPath, f"data\\{problem_name}")
 print("Loading .tsp file from path: " + tspFilePath)
 
 tsp = TSP(tspFilePath)
@@ -42,71 +42,107 @@ MAXIMUM_NUMBER_OF_ITERATIONS: int = 1000
 
 nodes: list[Node] = [Node(n[0], n[1]) for n in tsp.nodeCoords]
 
-ant_colony = AntColony(
-    number_of_ants=NUMBER_OF_ANTS,
-    alpha=ALPHA,
-    beta=BETA,
-    rho=RHO,
-    q=Q,
-    initial_pheromone_level=INITIAL_PHEROMONE_LEVEL,
-    maximum_number_of_iterations=MAXIMUM_NUMBER_OF_ITERATIONS,
-    nodes=nodes,
-)
 
-### Compute Shortest Path (animated output)
+### Setup results logging
+def log_results(
+    date_time: str,
+    run_number: int,
+    problem_name: str,
+    ellapsed_time: float,
+    ant_colony: AntColony,
+) -> None:
+    with open("results.txt", "a") as file:
+        file.write(f"DATE_TIME:{date_time}")
+        file.write(f"PROBLEM: {problem_name}\n")
+        file.write(f"RUN_NUMBER: {run_number}\n")
+        file.write(f"BEST_COST_FOUND: {ant_colony.best_tour_cost}\n")
+        file.write(f"TIME_TAKEN: {ellapsed_time}\n")
+        file.write(f"NUMBER_OF_ANTS: {NUMBER_OF_ANTS}\n")
+        file.write(f"ALPHA: {ALPHA}\n")
+        file.write(f"BETA: {BETA}\n")
+        file.write(f"RHO: {RHO}\n")
+        file.write(f"Q: {Q}\n")
+        file.write(f"INITIAL_PHEROMONE_LEVEL: {INITIAL_PHEROMONE_LEVEL}\n")
+        file.write(f"MAXIMUM_NUMBER_OF_ITERATIONS: {MAXIMUM_NUMBER_OF_ITERATIONS}\n")
+        file.write(f"BEST_TOUR: {ant_colony.best_tour}\n")
+        file.write("\n")
+
+
+### Compute Shortest Path (non-animated version)
 import time
-from matplotlib.animation import FuncAnimation
+import datetime
 
-plottedLines[0].set_data(x_values, y_values)  # type: ignore
+number_of_runs: int = 2
 
-
-def create_tour_coords(tour: list[int]) -> list[tuple[float, float]]:
-    coords: list[tuple[float, float]] = []
-    for n in tour:
-        coords.append((x_values[n], y_values[n]))
-
-    return coords
-
-
-def update_data(frame):  # type: ignore
-    ant_colony.iterate()
-    ellapsed_time: float = time.time() - start_time
-
-    tour_coords: list[tuple[float, float]] = create_tour_coords(ant_colony.best_tour)
-    x_tour_coords: list[float] = [c[0] for c in tour_coords]
-    y_tour_coords: list[float] = [c[1] for c in tour_coords]
-
-    plottedLines[0].set_data(x_tour_coords, y_tour_coords)
-    axis.set_title(
-        f"Iteration {frame + 1}\nTotal cost = {int(ant_colony.best_tour_cost)}\nTime ellapsed = {ellapsed_time:.1f}s"
+for r in range(number_of_runs):
+    ant_colony = AntColony(
+        number_of_ants=NUMBER_OF_ANTS,
+        alpha=ALPHA,
+        beta=BETA,
+        rho=RHO,
+        q=Q,
+        initial_pheromone_level=INITIAL_PHEROMONE_LEVEL,
+        maximum_number_of_iterations=MAXIMUM_NUMBER_OF_ITERATIONS,
+        nodes=nodes,
     )
 
-    return plottedLines[0]
+    start_time: float = time.time()
+    ellapsed_time: float = 0
+
+    for i in range(MAXIMUM_NUMBER_OF_ITERATIONS):
+        ant_colony.iterate()
+        ellapsed_time: float = time.time() - start_time
+
+        print(
+            f"Iteration {i + 1}; Total cost = {int(ant_colony.best_tour_cost)}; Time ellapsed = {ellapsed_time:.1f}s"
+        )
+
+    print(f"Best tour found: {ant_colony.best_tour}")
+    print(f"Best cost found: {ant_colony.best_tour_cost}")
+    log_results(
+        date_time=str(datetime.datetime.now()),
+        run_number=r,
+        problem_name=problem_name,
+        ellapsed_time=ellapsed_time,
+        ant_colony=ant_colony,
+    )
 
 
-funcAnimation = FuncAnimation(fig=figure1, func=update_data, frames=MAXIMUM_NUMBER_OF_ITERATIONS, interval=10, repeat=False)  # type: ignore
-
-start_time: float = time.time()
-plt.show()  # type: ignore
-
-print(f"Best tour found: {ant_colony.best_tour}")
-print(f"Best cost found: {ant_colony.best_tour_cost}")
-
-
-### Compute Shortest Path (console output)
+### Compute Shortest Path (animated output)
 # import time
+# from matplotlib.animation import FuncAnimation
 
-# start_time: float = time.time()
-# for i in range(MAXIMUM_NUMBER_OF_ITERATIONS):
-#     if i > MAXIMUM_NUMBER_OF_ITERATIONS:
-#         break
+# plottedLines[0].set_data(x_values, y_values)  # type: ignore
 
+
+# def create_tour_coords(tour: list[int]) -> list[tuple[float, float]]:
+#     coords: list[tuple[float, float]] = []
+#     for n in tour:
+#         coords.append((x_values[n], y_values[n]))
+
+#     return coords
+
+
+# def update_data(frame):  # type: ignore
 #     ant_colony.iterate()
 #     ellapsed_time: float = time.time() - start_time
 
-#     print(
-#         f"Iteration {i + 1}; Total cost = {int(ant_colony.best_tour_cost)}; Time ellapsed = {ellapsed_time:.1f}s"
+#     tour_coords: list[tuple[float, float]] = create_tour_coords(ant_colony.best_tour)
+#     x_tour_coords: list[float] = [c[0] for c in tour_coords]
+#     y_tour_coords: list[float] = [c[1] for c in tour_coords]
+
+#     plottedLines[0].set_data(x_tour_coords, y_tour_coords)
+#     axis.set_title(
+#         f"Iteration {frame + 1}\nTotal cost = {int(ant_colony.best_tour_cost)}\nTime ellapsed = {ellapsed_time:.1f}s"
 #     )
+
+#     return plottedLines[0]
+
+
+# funcAnimation = FuncAnimation(fig=figure1, func=update_data, frames=MAXIMUM_NUMBER_OF_ITERATIONS, interval=10, repeat=False)  # type: ignore
+
+# start_time: float = time.time()
+# plt.show()  # type: ignore
 
 # print(f"Best tour found: {ant_colony.best_tour}")
 # print(f"Best cost found: {ant_colony.best_tour_cost}")
@@ -132,10 +168,10 @@ print(f"Best cost found: {ant_colony.best_tour_cost}")
 
 # TODO
 # * log time taken
-# - log results to file
+# * log results to file
 # - log last best iteration
 # - fix last path of tour not plotted in animation
 # - improve algorithm to use ACS instead of AS
-# - add stopping criteria other that number of iterations
+# - add stopping criteria other than number of iterations
 # - plot known optimal path if available
 # - stretch goal: plot animated pheromone trails
